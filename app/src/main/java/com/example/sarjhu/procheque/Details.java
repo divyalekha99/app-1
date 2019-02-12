52ea5dd9b8c2bba8268fc3e3df3c3e694d8979a9
Soorya Prakash <sooryaprakash.r31@gmail.com>
2019-02-12 07:09:39 +0530

    second commit

--- app/src/main/java/com/example/sarjhu/procheque/Details.java
+++ app/src/main/java/com/example/sarjhu/procheque/Details.java
@@ -1,11 +1,14 @@
 package com.example.sarjhu.procheque;
 
 
+import android.app.DatePickerDialog;
+import android.app.Dialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.text.TextUtils;
 import android.view.View;
 import android.widget.Button;
+import android.widget.DatePicker;
 import android.widget.EditText;
 import android.widget.Toast;
 
@@ -15,28 +18,41 @@ import com.google.firebase.database.FirebaseDatabase;
 
 public class Details extends AppCompatActivity {
 
-    EditText date1,payto,words,nums,accno,cheq,micr,brno,two;
-    DatabaseReference mref = FirebaseDatabase.getInstance().getReference();;
-    FirebaseDatabase database = FirebaseDatabase.getInstance();;
+    EditText date1, payto, words, nums, accno, cheq, micr, brno, two;
+    DatabaseReference mref = FirebaseDatabase.getInstance().getReference();
+    FirebaseDatabase database = FirebaseDatabase.getInstance();
     Button button;
+    String date;
+    int year_x, month_x, day_x;
+    final static int dialog_id = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_details);
 
-        date1= findViewById(R.id.date1);
-        payto=findViewById(R.id.payto);
-        words=findViewById(R.id.words);
-        nums=findViewById(R.id.nums);
-        accno=findViewById(R.id.accno);
-        cheq=findViewById(R.id.cheqno);
-        micr=findViewById(R.id.micr);
-        brno=findViewById(R.id.brno);
-        two=findViewById(R.id.two);
+        date1 = findViewById(R.id.date1);
+        payto = findViewById(R.id.payto);
+        words = findViewById(R.id.words);
+        nums = findViewById(R.id.nums);
+        accno = findViewById(R.id.accno);
+        cheq = findViewById(R.id.cheqno);
+        micr = findViewById(R.id.micr);
+        brno = findViewById(R.id.brno);
+        two = findViewById(R.id.two);
         button = findViewById(R.id.ok);
 
         FirebaseApp.initializeApp(this);
+
+        date1.setOnClickListener(
+                new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        showDialog(dialog_id);
+                    }
+                }
+
+        );
 
         button.setOnClickListener(
                 new View.OnClickListener() {
@@ -50,9 +66,25 @@ public class Details extends AppCompatActivity {
 
     }
 
-    public void insert()
-    {
-        String textdate = date1.getText().toString().trim();
+    @Override
+    protected Dialog onCreateDialog(int id) {
+        if (id == dialog_id)
+            return new DatePickerDialog(this, dpickerlistener, year_x, month_x, day_x);
+        return null;
+    }
+
+    private DatePickerDialog.OnDateSetListener dpickerlistener = new DatePickerDialog.OnDateSetListener() {
+        @Override
+        public void onDateSet(DatePicker view, int year, int month, int dayOfMonth) {
+            year_x = year;
+            month_x = month;
+            day_x = dayOfMonth;
+            date = day_x + "/" + month_x + "/" + year_x;
+        }
+    };
+
+    public void insert() {
+        String textdate = date.toString().trim();
         String textpayto = payto.getText().toString().trim();
         String textwords = words.getText().toString().trim();
         Integer textnums = Integer.getInteger(nums.getText().toString());
@@ -63,19 +95,17 @@ public class Details extends AppCompatActivity {
         Integer texttwo = Integer.getInteger(two.getText().toString());
 
 
-        if(TextUtils.isEmpty(textpayto)){
+        if (TextUtils.isEmpty(textpayto)) {
             Toast.makeText(this, "Enter the name", Toast.LENGTH_LONG).show();
-        }
-        else
-        {
+        } else {
             String dbId = mref.push().getKey();
 
-            ManualDetails md = new ManualDetails (dbId, textdate, textpayto, textwords, textnums, textaccno, textcheq, textmicr, textbrno, texttwo);
+            ManualDetails md = new ManualDetails(dbId, textdate, textpayto, textwords, textnums, textaccno, textcheq, textmicr, textbrno, texttwo);
             mref.child(dbId).setValue(md);
 
-            Toast.makeText(this,"Values uploaded ", Toast.LENGTH_LONG).show();
+            Toast.makeText(this, "Values uploaded ", Toast.LENGTH_LONG).show();
 
         }
 
-    }
-}
+    }}
+
